382e832dd568b3e4cdfb0948f279d51fabb6f295
Nikolaos Kladis <nikcladis@gmail.com>
2023-12-07 21:17:45 +0200

    added global theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Button } from "@chakra-ui/react";
+
 function App() {
-  return <div>Instagram Clone</div>;
+  return (
+    <div>
+      <Button>Hello Nick</Button>
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+
+const styles = {
+  global: (props) => ({
+    body: {
+      bg: mode(props.colorMode === "light" ? "gray.100" : "#000"),
+      color: mode(props.colorMode === "light" ? "gray.800" : "whiteAlpha.900"),
+    },
+  }),
+};
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config, styles });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
