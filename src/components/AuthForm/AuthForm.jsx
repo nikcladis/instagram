dd12a7775d52ab5ec2b2aa4c5ace6467c9855290
Nikolaos Kladis <nikcladis@gmail.com>
2023-12-07 23:33:28 +0200

    updated auth form component

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import { Box, Image, Input, VStack } from "@chakra-ui/react";
+
 const AuthForm = () => {
-  return <div>AuthForm</div>;
+  const [isLogin, setIsLogin] = useState(false);
+
+  return (
+    <>
+      <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+        <VStack spacing={4}>
+          <Image src="/logo.png" h={24} cursor={"pointer"} alt="Instagram" />
+          <Input placeholder="Email" type="email" fontSize={14} />
+          <Input placeholder="Password" type="password" fontSize={14} />
+
+          {!isLogin ? (
+            <Input
+              placeholder="Confirm Password"
+              type="password"
+              fontSize={14}
+            />
+          ) : null}
+        </VStack>
+      </Box>
+    </>
+  );
 };
 
 export default AuthForm;
