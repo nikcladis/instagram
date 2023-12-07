8f9c40a3c6b1ef3975f4d775a6290dab7e984e67
Nikolaos Kladis <nikcladis@gmail.com>
2023-12-07 21:51:37 +0200

    fixed public images. added authpage image

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Box, Container, Flex, Image, VStack } from "@chakra-ui/react";
 
 const AuthPage = () => {
-  return <div>AuthPage</div>;
+  return (
+    <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
+      <Container maxW="md" padding={0}>
+        {/* Left hand-side */}
+        <Box display={{ base: "none", md: "block" }}>
+          <Image src="/auth.png" h={650} alt="Phone img" />
+        </Box>
+      </Container>
+    </Flex>
+  );
 };
 
 export default AuthPage;
