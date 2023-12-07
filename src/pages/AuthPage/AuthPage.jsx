76aed2f3ecbc05b3bedb46e3d1f95777d2f6956b
Nikolaos Kladis <nikcladis@gmail.com>
2023-12-07 22:39:36 +0200

    moved styles to objects for props in authpage

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 import { Box, Container, Flex, Image, VStack } from "@chakra-ui/react";
 
+const flexStyles = {
+  minH: "100vh",
+  justifyContent: "center",
+  alignItems: "center",
+  px: 4,
+};
+
+const containerStyles = {
+  maxW: "md",
+  padding: 0,
+};
+
+const boxStyles = {
+  display: {
+    base: "none",
+    md: "block",
+  },
+};
+
+const vStackStyles = {
+  spacing: 4,
+  align: "stretch",
+};
+
 const AuthPage = () => {
   return (
-    <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
-      <Container maxW="md" padding={0}>
-        {/* Left hand-side */}
-        <Box display={{ base: "none", md: "block" }}>
+    <Flex {...flexStyles}>
+      <Container {...containerStyles}>
+        <Box {...boxStyles}>
           <Image src="/auth.png" h={650} alt="Phone img" />
         </Box>
       </Container>
