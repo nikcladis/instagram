dabe0889e21db9afada64771e98cad172e0572a3
Nikolaos Kladis <nikcladis@gmail.com>
2023-12-08 16:14:10 +0200

    updated sidebar component and added asses constants

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,46 @@
+import { Box, Flex, Link } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import { InstagramLogo, InstagramMobileLogo } from "../../assets/constants";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <Box
+      height={"100vh"}
+      borderRight={"1px solid"}
+      borderColor={"whiteAlpha.300"}
+      position={"sticky"}
+      top={0}
+      left={0}
+      py={8}
+      px={{ base: 2, md: 4 }}
+    >
+      <Flex w={"full"} height={"full"} direction={"column"} gap={10}>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          pl={2}
+          display={{ base: "none", md: "block" }}
+          cursor={"pointer"}
+        >
+          <InstagramLogo />
+        </Link>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          p={2}
+          display={{ base: "block", md: "none" }}
+          cursor={"pointer"}
+          borderRadius={6}
+          _hover={{
+            bg: "whiteAlpha.200",
+          }}
+          w={10}
+        >
+          <InstagramMobileLogo />
+        </Link>
+      </Flex>
+    </Box>
+  );
 };
 
 export default Sidebar;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,7 @@
+import { Box } from "@chakra-ui/react";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <Box height={"100vh"}>HomePage</Box>;
 };
 
 export default HomePage;
