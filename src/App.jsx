3a3151149a3b552f94265f1388bc01b4d3232364
Nikolaos Kladis <nikcladis@gmail.com>
2023-12-08 14:17:52 +0200

    wrapped my app inside the page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { Route, Routes } from "react-router-dom";
+import PageLayout from "./layouts/PageLayout/PageLayout";
 import AuthPage from "./pages/AuthPage/AuthPage";
 import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
-    <>
+    <PageLayout>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
       </Routes>
-    </>
+    </PageLayout>
   );
 }
 
--- src/layouts/PageLayout/PageLayout.jsx
+++ src/layouts/PageLayout/PageLayout.jsx
@@ -1,15 +1,22 @@
 import { Box, Flex } from "@chakra-ui/react";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import { useLocation } from "react-router-dom";
 
 const PageLayout = ({ children }) => {
+  const { pathname } = useLocation();
+
   return (
     <Flex>
       {/* Sidebar on the left*/}
-      <Box w={{ base: "70px", md: "240px" }}>
-        <Sidebar />
+      {pathname !== "/auth" ? (
+        <Box w={{ base: "70px", md: "240px" }}>
+          <Sidebar />
+        </Box>
+      ) : null}
+      ;{/* Content on the right*/}
+      <Box flex={1} w={{ base: "calc(100% - 70px)", md: "calc(100% - 240px)" }}>
+        {children}
       </Box>
-      {/* Content on the right*/}
-      <Box>{children}</Box>
     </Flex>
   );
 };
