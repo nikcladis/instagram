f1ff7215651db04478c18f2b52120b9b8c2612ec
Nikolaos Kladis <nikcladis@gmail.com>
2023-12-12 13:38:27 +0200

    added useShowToast for errors when user creates account

--- src/hooks/useSignUpWithEmailAndPassword.js
+++ src/hooks/useSignUpWithEmailAndPassword.js
@@ -1,6 +1,7 @@
 import { auth, firestore } from "../firebase/firebase";
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { doc, setDoc } from "firebase/firestore";
+import useShowToast from "./useShowToast";
 
 const useSignUpWithEmailAndPassword = () => {
     const [
@@ -10,19 +11,23 @@ const useSignUpWithEmailAndPassword = () => {
         error,
     ] = useCreateUserWithEmailAndPassword(auth);
 
+    const showToast = useShowToast();
+
     const signup = async ({ email, password, username, fullName }) => {
 
         if (!email || !password || !username || !fullName) {
-            console.log("Missing Fields");
+            showToast("Error", "Please fill all the fields", "error");
             return;
         }
 
         try {
             const newUser = await createUserWithEmailAndPassword(email, password);
+
             if (!newUser && error) {
-                console.log(error);
+                showToast("Error", error.message, "error")
                 return;
             }
+
             if (newUser) {
                 const userDoc = {
                     uid: newUser.user.uid,
@@ -36,11 +41,13 @@ const useSignUpWithEmailAndPassword = () => {
                     posts: [],
                     createdAt: Date.now()
                 }
+
                 await setDoc(doc(firestore, "users", newUser.user.uid), userDoc);
+
                 localStorage.setItem("user-info", JSON.stringify(userDoc));
             }
         } catch (error) {
-            console.error('Error during signup:', error);
+            showToast("Error", error.message, "error")
         }
     }
 
