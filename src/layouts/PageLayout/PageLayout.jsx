4a8f280c9979f434fb5e5b7b7522ef77f3773b2a
nikcladis <nikcladis@gmail.com>
2023-12-22 21:19:29 +0200

    added Navbar component

--- src/layouts/PageLayout/PageLayout.jsx
+++ src/layouts/PageLayout/PageLayout.jsx
@@ -1,24 +1,32 @@
 import { Box, Flex } from "@chakra-ui/react";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import Navbar from "../../components/Navbar/Navbar";
 import { useLocation } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/firebase";
 
 const PageLayout = ({ children }) => {
   const { pathname } = useLocation();
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
   const canRenderSidebar = pathname !== "auth" && user;
+  const canRenderNavbar = !user && !loading && pathname !== "/auth";
 
   return (
-    <Flex>
+    <Flex flexDir={canRenderNavbar ? "column" : "row"}>
       {/* Sidebar on the left*/}
       {canRenderSidebar ? (
         <Box w={{ base: "70px", md: "240px" }}>
           <Sidebar />
         </Box>
       ) : null}
+      {/* Navbar */}
+      {canRenderNavbar ? <Navbar /> : null}
       {/* Content on the right*/}
-      <Box flex={1} w={{ base: "calc(100% - 70px)", md: "calc(100% - 240px)" }}>
+      <Box
+        flex={1}
+        w={{ base: "calc(100% - 70px)", md: "calc(100% - 240px)" }}
+        mx={"auto"}
+      >
         {children}
       </Box>
     </Flex>
