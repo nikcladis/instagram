f657072f898a6b15f08c7d406ba4aa63fff5ddd1
nikcladis <nikcladis@gmail.com>
2023-12-22 19:13:27 +0200

    added logoutUser functionality

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,18 +1,20 @@
 import { useSignOut } from "react-firebase-hooks/auth";
 import { auth } from "../firebase/firebase";
+import useAuthStore from "../store/authStore";
 import useShowToast from "./useShowToast";
 
 const useLogout = () => {
 
     const [signOut, loading, error] = useSignOut(auth);
     const showToast = useShowToast();
+    const logoutUser = useAuthStore(state => state.logout);
 
     const handleLogout = async () => {
 
         try {
             await signOut();
             localStorage.removeItem("user-info");
-
+            logoutUser();
         } catch (error) {
             showToast("Error", error.message, "error")
         }
