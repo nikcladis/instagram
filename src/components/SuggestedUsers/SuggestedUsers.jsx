6f87f5211b3a661d92c360077fcd9fc52df4199c
nikcladis <nikcladis@gmail.com>
2023-12-29 14:48:10 +0200

    implemented suggesting users in SuggestedUsers component

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,41 +1,42 @@
 import { Flex, VStack, Text, Box, Link } from "@chakra-ui/react";
+import useGetSuggestedUsers from "../../hooks/useGetSuggestedUsers";
 import SuggestedHeader from "./SuggestedHeader";
 import SuggestedUser from "./SuggestedUser";
 
 const SuggestedUsers = () => {
+  const { isLoading, suggestedUsers } = useGetSuggestedUsers();
+
   return (
     <VStack py={8} px={6} gap={4}>
       <SuggestedHeader />
-      <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
-        <Text fontSize={"sm"} fontWeight={"bold"} color={"gray.500"}>
-          Suggested for you
-        </Text>
-        <Text
-          fontSize={"sm"}
-          fontWeight={"bold"}
-          _hover={{ color: "gray.400" }}
-          cursor={"pointer"}
-        >
-          See all
-        </Text>
-      </Flex>
 
-      <SuggestedUser
-        name="Dan Abrahmov"
-        followers={1392}
-        avatar="https://bit.ly/dan-abramov"
-      />
-      <SuggestedUser
-        name="Ryan Florence"
-        followers={562}
-        avatar="https://bit.ly/ryan-florence"
-      />
-      <SuggestedUser
-        name="Christian Mwamba"
-        followers={789}
-        avatar="https://bit.ly/code-beast"
-      />
+      {isLoading
+        ? suggestedUsers.length !== 0 && (
+            <Flex
+              alignItems={"center"}
+              justifyContent={"space-between"}
+              w={"full"}
+            >
+              <Text fontSize={"sm"} fontWeight={"bold"} color={"gray.500"}>
+                Suggested for you
+              </Text>
+              <Text
+                fontSize={"sm"}
+                fontWeight={"bold"}
+                _hover={{ color: "gray.400" }}
+                cursor={"pointer"}
+              >
+                See all
+              </Text>
+            </Flex>
+          )
+        : null}
 
+      {isLoading
+        ? suggestedUsers.map((user) => (
+            <SuggestedUser key={user.uid} user={user} />
+          ))
+        : null}
       <Box alignSelf={"flex-start"} fontSize={12} color={"gray.500"} mt={5}>
         © 2023 Built by{" "}
         <Link
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -15,7 +15,7 @@ const HomePage = () => {
           display={{ base: "none", lg: "block" }}
           maxW={"300px"}
         >
-          {/* <SuggestedUsers /> */}
+          <SuggestedUsers />
         </Box>
       </Flex>
     </Container>
