e99684cdec6bd630ca5c1aba852af96d13c44286
nikcladis <nikcladis@gmail.com>
2023-12-29 15:01:21 +0200

    fixed suggested users not showing up correctly

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -10,33 +10,34 @@ const SuggestedUsers = () => {
     <VStack py={8} px={6} gap={4}>
       <SuggestedHeader />
 
-      {isLoading
+      {!isLoading
         ? suggestedUsers.length !== 0 && (
-            <Flex
-              alignItems={"center"}
-              justifyContent={"space-between"}
-              w={"full"}
-            >
-              <Text fontSize={"sm"} fontWeight={"bold"} color={"gray.500"}>
-                Suggested for you
-              </Text>
-              <Text
-                fontSize={"sm"}
-                fontWeight={"bold"}
-                _hover={{ color: "gray.400" }}
-                cursor={"pointer"}
+            <>
+              <Flex
+                alignItems={"center"}
+                justifyContent={"space-between"}
+                w={"full"}
               >
-                See all
-              </Text>
-            </Flex>
+                <Text fontSize={"sm"} fontWeight={"bold"} color={"gray.500"}>
+                  Suggested for you
+                </Text>
+                <Text
+                  fontSize={"sm"}
+                  fontWeight={"bold"}
+                  _hover={{ color: "gray.400" }}
+                  cursor={"pointer"}
+                >
+                  See all
+                </Text>
+              </Flex>
+
+              {suggestedUsers.map((user) => (
+                <SuggestedUser key={user.uid} user={user} />
+              ))}
+            </>
           )
         : null}
 
-      {isLoading
-        ? suggestedUsers.map((user) => (
-            <SuggestedUser key={user.uid} user={user} />
-          ))
-        : null}
       <Box alignSelf={"flex-start"} fontSize={12} color={"gray.500"} mt={5}>
         © 2023 Built by{" "}
         <Link
