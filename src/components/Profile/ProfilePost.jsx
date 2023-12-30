f2534343c0727f0f57214a6617bb98c3da5bf558
nikcladis <nikcladis@gmail.com>
2023-12-30 20:39:30 +0200

    fixed name of function decrementPostCount to be descriptive

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -18,9 +18,7 @@ const ProfilePost = ({ post }) => {
   const showToast = useShowToast();
   const [isDeleting, setIsDeleting] = useState(false);
   const deletePost = usePostStore((state) => state.deletePost);
-  const deletePostFromProfileStore = useUserProfileStore(
-    (state) => state.deletePost
-  );
+  const decrementPostsCount = useUserProfileStore((state) => state.deletePost);
 
   const handleDeletePost = async () => {
     if (!window.confirm("Are you sure you want to delete this post?")) return;
@@ -37,7 +35,7 @@ const ProfilePost = ({ post }) => {
       });
 
       deletePost(post.id);
-      deletePostFromProfileStore(post.id);
+      decrementPostsCount(post.id);
       showToast("Success", "Post deleted successfully", "success");
     } catch (error) {
       showToast("Error", error.message, "error");
